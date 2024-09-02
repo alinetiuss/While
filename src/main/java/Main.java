ffbdae74da8a0de8c31aa7c19a6098e3d56d1a15
AlineTiuss <169068304+alinetiuss@users.noreply.github.com>
2024-09-02 12:28:18 +0000

    Initial commit

---
+++ src/main/java/Main.java
@@ -0,0 +1,15 @@
+
+
+public class Main {
+  public static void main(String[] args) {
+  int contador = 1;
+  String nome = "Aline";
+    
+while(contador < 11) {
+  System.out.println("Bom dia!");
+  contador = contador + 1;
+}
+    System.out.println(nome);
+    
+  } 
+}
